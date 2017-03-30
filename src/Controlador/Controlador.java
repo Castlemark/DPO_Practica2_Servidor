be8ff64aa16b2256f7b43cf3b0dc0228bedf3625
Andreu <andysalzano@hotmail.es>
2017-03-30 08:52:28 +0200

    HOLA

---
+++ src/Controlador/Controlador.java
@@ -0,0 +1,10 @@
+package Controlador;
+
+/**
+ * Created by Andreu on 30/03/2017.
+ */
+public class Controlador {
+
+    public Usuari usuari;
+    
+}
--- src/Controlador/Main.java
+++ src/Controlador/Main.java
@@ -4,4 +4,5 @@ package Controlador;
  * Created by Marc on 30/03/2017.
  */
 public class Main {
+
 }
