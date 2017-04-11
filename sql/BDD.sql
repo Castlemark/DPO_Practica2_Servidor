3ee2564e7c225e825a6cf14443617dc4f920e541
Tatiana <tatiana.caceres.2015@salleurl.edu>
2017-04-11 13:08:58 +0200

    Merge branch 'master' of C:\Users\sullivan\IdeaProjects\DPO_Practica2_Servidor with conflicts.

--- sql/BDD.sql
+++ sql/BDD.sql
@@ -4,26 +4,26 @@ USE Troner;
 
 CREATE TABLE Usuari(
 	login VARCHAR(511),
-    mail VARCHAR(511),
-    contrasenya VARCHAR(511),
-    punts INT,
-    data_registre DATE,
-    data_ultimacces DATE,
-    PRIMARY KEY (login, mail)
+  mail VARCHAR(511),
+  contrasenya VARCHAR(511),
+  punts INT,
+  data_registre DATE,
+  data_ultimacces DATE,
+  PRIMARY KEY (login, mail)
 );
 
 CREATE TABLE Partida(
-	id_partida BIGINT UNSIGNED AUTO_INCREMENT,
+	id_partida BIGINT UNSIGNED auto_increment,
 	data_partida DATE,
-    PRIMARY KEY (id_partida)
+  PRIMARY KEY (id_partida)
 );
 
 CREATE TABLE Usuari_partida(
 	id_jugador VARCHAR(255),
-    id_partida BIGINT UNSIGNED,
-    puntuacio INT,
-    FOREIGN KEY (id_jugador) REFERENCES Usuari(login),
-    FOREIGN KEY (id_partida) REFERENCES Partida(id_partida)
+  id_partida BIGINT UNSIGNED,
+  puntuacio INT,
+  FOREIGN KEY (id_jugador) REFERENCES Usuari(login)
+  FOREIGN KEY (id_partida) REFERENCES Partida(id_partida),
 );
 
 
