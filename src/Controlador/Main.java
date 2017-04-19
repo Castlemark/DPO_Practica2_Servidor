42b33a67af24b0c4b33b9cf5b1e49f645eb55faf
Propietario <pau.nonell@gmail.com>
2017-04-19 09:25:01 +0200

    Servidor

--- src/Controlador/Main.java
+++ src/Controlador/Main.java
@@ -2,12 +2,20 @@ package Controlador;
 
 import Vista.*;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
 import javax.swing.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.LinkedList;
 
 /**
  * Created by Marc on 30/03/2017.
  */
 public class Main {
+    private static LinkedList<Socket> sockets;
+
     public static void main (String[] args) {
 
         SwingUtilities.invokeLater(new Runnable() {
@@ -15,9 +23,21 @@ public class Main {
             public void run() {
                 /*VistaServidor vista = new VistaServidor();
                 vista.setVisible(true);*/
-
-                Missatge vista = new Missatge ();
+/*
+                Missatge vista = new Missatge();
                 vista.setVisible(true);
+*/
+                try {
+                    sockets = new LinkedList<>();
+                    ServerSocket sServer = new ServerSocket(10000);
+                    while (true) {
+                        Socket sClient = sServer.accept();
+                        sockets.add(sClient);
+                        (new ThreadClient(sClient, sockets)).start();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
