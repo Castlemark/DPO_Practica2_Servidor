5c890b80cfebabfd5e39eecbd8c083dec474301e
Tatiana <tatiana.caceres.2015@salleurl.edu>
2017-04-19 09:04:24 +0200

    Vistes

--- src/Vista/Grafic_usuari.java
+++ src/Vista/Grafic_usuari.java
@@ -1,15 +1,26 @@
 package Vista;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by sullivan on 19/04/2017.
  */
 public class Grafic_usuari extends JFrame {
     private JPanel jpGrafic;
-    private JPanel jpTitle;
-    private JPanel jpPuntuacio;
     private JLabel jlTitle;
-    private JButton jbTancar;
     private JFreeChart jfPuntacio;
+
+    private String login;
+
+    public Grafic_usuari() {
+        this.setTitle ("Graphic usuari");
+        this.setSize (350,350);
+        this.setResizable(true);
+
+        jpGrafic.setLayout(new GridLayout(2,1));
+        jlTitle = new JLabel(login);
+        jfPuntacio = ;
+
+    }
 }
