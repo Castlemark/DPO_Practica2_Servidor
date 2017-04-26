31363b9cbe60bbcda1ab91c84f3379a794990fc5
Pol <polfmarti@gmail.com>
2017-04-26 09:07:00 +0200

    JavaDoc

--- src/Vista/ButtonColumn.java
+++ src/Vista/ButtonColumn.java
@@ -7,6 +7,8 @@ import javax.swing.border.*;
 import javax.swing.table.*;
 
 /**
+ *  Classe que introdueix un botó en la fila de la taula per esborrar els usuaris
+ *
  *  The ButtonColumn class provides a renderer and an editor that looks like a
  *  JButton. The renderer and editor will then be used for a specified column
  *  in the table. The TableModel will contain the String to be displayed on
