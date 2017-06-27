25f63cf495d0e6e458ae8efd8457065db92383c8
Castlemark <marc.castells.guell@gmail.com>
2017-06-27 11:08:47 +0200

    fent vistes

--- src/Client_Servidor/Server.java
+++ src/Client_Servidor/Server.java
@@ -1,7 +1,54 @@
 package Client_Servidor;
 
+import Controlador.GestionarPartides;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+
 /**
  * Created by Marc on 27/06/2017.
  */
-public class Server {
+public class Server extends Thread{
+
+    private ServerSocket  sSocket;
+    private List<DedicatedServer> dedicatedServers;
+
+    private GestionarPartides gestionarPartides;
+
+    private boolean running;
+    private int port;
+
+    public Server (int port, GestionarPartides gestionarPartides){
+        this.port = port;
+        this.gestionarPartides = gestionarPartides;
+    }
+
+    public void startServer () throws IOException{
+
+        sSocket = new ServerSocket(port);
+        running = true;
+        start();
+    }
+
+    public void stopServer(){
+        running = false;
+        for(DedicatedServer d: dedicatedServers){
+            d.stopRunning();
+        }
+    }
+
+    @Override
+    public void run(){
+        while (running) try{
+            Socket socket = sSocket.accept();
+            DedicatedServer dServer = new DedicatedServer( socket, gestionarPartides);
+            dedicatedServers.add(dServer);
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
