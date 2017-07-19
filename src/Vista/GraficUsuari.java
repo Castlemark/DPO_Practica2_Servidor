ca270e20791e8d0c4dafb7127cb1496d54056618
Pol <polfmarti@gmail.com>
2017-07-19 18:57:37 +0200

    tancar sessio

--- src/Vista/GraficUsuari.java
+++ src/Vista/GraficUsuari.java
@@ -18,7 +18,7 @@ import javax.swing.SwingUtilities;
 
 /*
  * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor
  */
 /**
  *
